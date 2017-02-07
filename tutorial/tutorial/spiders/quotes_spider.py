c79c3e168d6c9aa94bb2f6d2d206eb045dcb3459
Cega <cega@cega.cega>
2017-02-07 18:04:12 +0100

    Add remove HTML tag for all type of element to evaluate

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -7,6 +7,7 @@ from scrapy.utils.markup import remove_tags
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     coucou = 'Vachement super'
+    d_struct_elem = {}
     d_urls = {}
     l_100 = ['adulthood', 'inspirational', 'miracle', 'miracles', 'aliteracy', 'deep-thoughts', 'thinking', 'abilities', 'paraphrased', 'simile']
     l_50 = ['world', 'success', 'value', 'life', 'live', 'books', 'classic', 'humor', 'change', 'choices', 'love', 'truth']
@@ -25,40 +26,64 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def remove_html_tags(self, data):
+        ret = remove_tags(data)
+
+        ret = re.sub('\\\t|\\n|\\t|\\r| {2,}', ' ', ret)
+
+ #       self.log('$$$$$$$$$$$$$$\n{0}\n$$$$$$$$$$$$$$'.format(type(ret)))
+
+        return ret.strip()
+
     def parse(self, response):
         page = response.url.split("/")[-2]
-        link_url = response.xpath('//a/@href').extract()
+
+        self.d_struct_elem['link_url'] = response.xpath('//a/@href').extract()
 #        link_text = response.xpath('//a/text()').extract() #recupere tous les textes de liens
-        link_text = response.xpath('//a/*|//a/text()').extract() # recupere tous ce qui dans la balise texte ou pas
+        self.d_struct_elem['link_text'] = response.xpath('//a/*|//a/text()').extract() # recupere tous ce qui dans la balise texte ou pas
         
-        h1 = response.xpath('//h1/text()|//h1/*').extract()
+        self.d_struct_elem['h1'] = response.xpath('//h1/text()|//h1/*').extract()
 
-        h2 = response.xpath('//h2/text()|//h2/*').extract()
+        self.d_struct_elem['h2'] = response.xpath('//h2/text()|//h2/*').extract()
 
-        h3 = response.xpath('//h3/text()|//h3/*').extract()
+        self.d_struct_elem['h3'] = response.xpath('//h3/text()|//h3/*').extract()
 
-        h4 = response.xpath('//h4/text()|//h4/*').extract()
+        self.d_struct_elem['h4'] = response.xpath('//h4/text()|//h4/*').extract()
 
-        h5 = response.xpath('//h5/text()|//h5/*').extract()
+        self.d_struct_elem['h5'] = response.xpath('//h5/text()|//h5/*').extract()
 
-        h6 = response.xpath('//h6/text()|//h6/*').extract()
+        self.d_struct_elem['h6'] = response.xpath('//h6/text()|//h6/*').extract()
 
-        li = response.xpath('//li/text()|//li/*').extract()
-        td = response.xpath('//td/text()|//td/*').extract()
-        th = response.xpath('//th/text()|//th/*').extract()
+        self.d_struct_elem['li'] = response.xpath('//li/text()|//li/*').extract()
+        self.d_struct_elem['td'] = response.xpath('//td/text()|//td/*').extract()
+        self.d_struct_elem['th'] = response.xpath('//th/text()|//th/*').extract()
 
         body = response.xpath('//body').extract()
+        #['body'] = body[0].encode('utf8')
+        self.d_struct_elem['body'] = body
 
         title = response.xpath('//title/text()|//title/*').extract()
-        title = title[0]
+        self.d_struct_elem['title'] = title
 
-        select = response.xpath('//option/@value|//option/text()').extract()
+        self.d_struct_elem['select'] = response.xpath('//option/@value|//option/text()').extract()
 
+        self.d_struct_elem['meta'] = response.xpath('//meta[@name="description"]/@content|//meta[@name="keywords"]/@content').extract()
 
+        l_keys = self.d_struct_elem.keys()
+
+        for key in l_keys:
+            l_value_wo_html_tag = []
+            for value in self.d_struct_elem[key]:
+                l_value_wo_html_tag.append(self.remove_html_tags(value))
+            self.d_struct_elem[key] = l_value_wo_html_tag
+
+        self.log('************\n{0} {1}\n************'.format(response.url, self.d_struct_elem))
+
+'''
         if select :
             self.log('************\n{0} {1}\n************'.format(response.url, select))
 
-        body = body[0].encode('utf8')
+        
 
         find = re.search('truth', body)
 
@@ -71,12 +96,13 @@ class QuotesSpider(scrapy.Spider):
 ## <meta name="description" content="Free Web tutorials">
 ## <meta name="keywords" content="HTML,CSS,XML,JavaScript">
 #####
-        meta = response.xpath('//meta[@name="description"]/@content|//meta[@name="keywords"]/@content').extract()
+        
 
 
         self.log('############\n{0}\n############'.format(meta))
 
         filename = 'quotes-%s.html' % page
+'''
 #       yield '************\n{0}\n************'.format(link_text)
 #        with open(filename, 'wb') as f:
 #            f.write(body.encode('utf8'))
