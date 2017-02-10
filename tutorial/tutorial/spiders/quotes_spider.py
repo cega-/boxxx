910993e9fc3b15bae8f83d8160118e0f3911b867
Cega <cega@cega.cega>
2017-02-10 19:09:20 +0100

    Add regexp to martch complex terme like 'world of success' Add lower case

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,6 +1,7 @@
 import re
 import scrapy
 #import tldextract get only domain extract from url no matter subdomain
+#import chardet detect encoding
 
 from scrapy.utils.markup import replace_tags, replace_escape_chars
 
@@ -10,7 +11,9 @@ class QuotesSpider(scrapy.Spider):
     d_struct_elem = {}
     d_urls = {}
     l_100 = ['adulthood', 'inspirational', 'miracle', 'miracles', 'aliteracy', 'deep-thoughts', 'thinking', 'abilities', 'paraphrased', 'simile']
-    l_50 = ['world', 'success', 'value', 'life', 'live', 'books', 'classic', 'humor', 'change', 'choices', 'love', 'truth']
+    l_50 = ['world', 'success', 'value', 'life', 'live', 'books', 'classic', 'humor', 'change', 'choices', 'love', 'truth', 'milk', 'parc']
+    l_100_complex = ['adulthood is a miracle']
+    l_50_complex = ['world of success']
     l_black_list = []
 
     def start_requests(self):
@@ -36,6 +39,99 @@ class QuotesSpider(scrapy.Spider):
  #       self.log('$$$$$$$$$$$$$$\n{0}\n$$$$$$$$$$$$$$'.format(type(ret)))
 
         return ret.strip()
+
+    def calc_wght_text(self):
+        body = self.d_struct_elem['body'][0]
+        l_text_words = body.split()
+        nb_word_in_text = 0
+        nb_word_in_l_50 = 0
+        nb_word_in_l_100 = 0
+        weight = 0
+            
+        l_100_in_text = set(self.l_100)
+        l_50_in_text = set(self.l_50)
+
+        for complex_100 in self.l_100_complex:
+            complex_100 = complex_100.split(' ')
+            s_regexp = ''
+            length = len(complex_100)
+            for word in complex_100:
+                length -= 1
+                if length:
+                    s_regexp += word+'[ \-_]?'
+                else:
+                    s_regexp += word
+            find_complex_100 = re.findall(s_regexp, body)
+            if find_complex_100:
+                weight += 100 * len(find_complex_100)
+                self.log('************\n{0}\n************'.format(weight))
+
+        for complex_50 in self.l_50_complex:
+            complex_50 = complex_50.split(' ')
+            s_regexp = ''
+            length = len(complex_50)
+            for word in complex_50:
+                length -= 1
+                if length:
+                    s_regexp += word+'[ \-_]?'
+                else:
+                    s_regexp += word
+            find_complex_50 = re.findall(s_regexp, body)
+            if find_complex_50:
+                weight += 50 * len(find_complex_50)
+                self.log('************\n{0}\n************'.format(weight))
+
+        for word in l_text_words:
+            word = word.lower()
+            if word in l_100_in_text :
+                weight += 100
+                nb_word_in_l_100 += 1
+            elif word in l_50_in_text:
+                weight += 50
+                nb_word_in_l_50 += 1
+            nb_word_in_text += 1
+
+        if 100*(nb_word_in_l_100+nb_word_in_l_50)/nb_word_in_text >= 10 or nb_word_in_text <= 100:
+            return weight
+        else :
+            return 0 
+
+    def calc_wght(self, l_elem):
+        l_100_in_text = set(self.l_100)
+        l_50_in_text = set(self.l_50)
+        weight = 0
+###
+# Mettre regexp pour match approx du mot
+###
+        for elem in l_elem:
+            for split_elem in elem.split(' '):
+                split_elem = split_elem.lower()
+                if split_elem in l_100_in_text :
+                    weight += 100
+                elif split_elem in l_50_in_text:
+                    weight += 50
+
+#        self.log('************\n{0} {1}\n************'.format(weight, l_elem))
+        return weight
+
+    def calc_wght_struct(self):
+        l_elem_struct = [{'elem': 'h1', 'factor': 1.8}, {'elem': 'h2', 'factor': 1.7}, {'elem': 'h3', 'factor': 1.6}, {'elem': 'h4', 'factor': 1.5}, {'elem': 'h5', 'factor': 1.4}, {'elem': 'h6', 'factor': 1.3}, {'elem': 'li', 'factor': 1.5}, {'elem': 'td', 'factor': 1.5}, {'elem': 'th', 'factor': 1.5}, {'elem': 'title', 'factor': 1.9}, {'elem': 'select', 'factor': 1.7}, {'elem': 'meta', 'factor': 1.8}]
+        weight = 0
+
+        for struct in l_elem_struct:
+            c_weight = self.calc_wght(self.d_struct_elem[struct['elem']])
+            if c_weight != 0:
+                weight += c_weight * struct['factor']
+
+        return weight
+
+    def calc_wght_link(self):
+        #l_elem_link = ['link_url', 'link_text']
+        #2
+        #1.8
+        #1.5
+        #'toto[ \-_]?titi[ \-_]?tutu'
+        pass
 
     def parse(self, response):
         page = response.url.split("/")[-2]
@@ -79,7 +175,13 @@ class QuotesSpider(scrapy.Spider):
                 l_value_wo_html_tag.append(self.remove_html_tags(value))
             self.d_struct_elem[key] = l_value_wo_html_tag
 
-        self.log('************\n{0} {1}\n************'.format(response.url, self.d_struct_elem))
+#        self.log('************\n{0} {1}\n************'.format(response.url, self.d_struct_elem))
+
+        wght_text = self.calc_wght_text()
+        wght_struct = self.calc_wght_struct()
+        wght_link = self.calc_wght_link()
+
+        self.log('************\n{0} {1}\n************'.format(response.url, wght_text))
 
 '''
         if select :
